7acf0cbe251536aaa37512c4e018f10445104656
James Watling <james.watling@gmail.com>
2026-05-25 16:32:57 +0100

    Initial Commit

---
+++ CremaTests/CremaTests.swift
@@ -0,0 +1,17 @@
+//
+//  CremaTests.swift
+//  CremaTests
+//
+//  Created by James Watling on 25/05/2026.
+//
+
+import Testing
+@testable import Crema
+
+struct CremaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
